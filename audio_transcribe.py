3a7ef07e16224a5f9790c9dab06bdbb5fb86c7f9
iamdumonde <martinaboumonde@gmail.com>
2024-01-11 21:14:58 +0100

    modif

--- audio_transcribe.py
+++ audio_transcribe.py
@@ -15,14 +15,34 @@ with sr.AudioFile(AUDIO_FILE) as source:
     # prends le temps de lire l'entièreté de
     audio = r.record(source) 
 
+ #trouver un nom pour le fichier txt
+txt_file = AUDIO_FILE.replace(".wav", ".txt")
+print(txt_file)
+    
 # recognize speech using Google Speech Recognition
 try:
     # utilisation du clé d'api par défaut de google avec la fonction r.recognize_google()
     # l'argument language="fr-FR" spécifie juste que l'audio est en français
-    print("Résulat de SpeechRecognition : " + r.recognize_google(audio, language="fr-FR"))
+    text = r.recognize_google(audio, language="fr-FR")
+    print("Résulat de SpeechRecognition : " + text)
+    
+    # #vérification si le fichier .txt existe ou non
+    if path.exists(txt_file):
+        #ouverture du fichier .txt pour mise à jour
+        with open(txt_file, "a") as f:
+            #Ecrire le texte transcrit dans le fichier txt
+            f.write(text + "\n")
+        print('exists')
+    else:
+        #créer un nouveau fichier 
+        with open(txt_file, "w") as f:
+            f.write(text)
+        print('existsno')
 
 # permet de faire ressortir les erreurs éventuelles
 except sr.UnknownValueError:
     print("Speech Recognition ne comprend pas l'audio")
 except sr.RequestError as e:
     print("Erreur lors du contact avec l'API; {0}".format(e))
+    
+### création d'un fichier txt
